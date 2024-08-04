73936e8587eb274d6b20e242da8742bc77d356e7
Pablo Garay <elxebec@gmail.com>
2024-08-04 20:20:32 -0300

    Changed loading message

--- dream_match/src/components/molecules/Loading.tsx
+++ dream_match/src/components/molecules/Loading.tsx
@@ -5,13 +5,14 @@ import Image from "next/image";
 const Loading = () => {
   return (
     <div className="flex justify-center items-center h-full bg-white">
-        <Image
+        {/* <Image
           src="/ballon.png"
           alt="loading"
           height={50}
           width={50}
           className="bounce-animation"
-        />
+        /> */}
+        <p>Loading...</p>
     </div>
   );
 };
